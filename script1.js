3bd6d8d9dfc48efc6d41790eb2cfc4dd71408352
CarOrLa03 <carmenorla03@gmail.com>
2023-10-14 11:28:43 +0200

    buscar en arrays

--- script1.js
+++ script1.js
@@ -97,7 +97,91 @@ function procesarPedido(pedidos) {
     pedidos.push(cliente)
     console.log(pedidos)
 }
-  procesarPedido(['Carmen', 'Hamburguesa'])
+procesarPedido(['Carmen', 'Hamburguesa'])
+  
+// BUSCAR EN ARRAYS 
+
+const libros = [
+    { titulo: "El Hobbit", paginas: 310 },
+    { titulo: "Cien años de soledad", paginas: 417 },
+    { titulo: "1984", paginas: 328 },
+    { titulo: "Don Quijote de la Mancha", paginas: 863 },
+    { titulo: "Harry Potter y la piedra filosofal", paginas: 223 },
+    { titulo: "Matar un ruiseñor", paginas: 281},
+    { titulo: "Crimen y castigo", paginas: 671 },
+    { titulo: "Las aventuras de Sherlock Holmes", paginas: 307 },
+    { titulo: "El Señor de los Anillos: La Comunidad del Anillo", paginas: 423 },
+    { titulo: "El Gran Gatsby", paginas: 180 }
+  ];
+function minMaxPagLibros(array) { 
+    let tituloLibroMasPaginas = ''
+    let tituloLibroMenosPaginas = ''
+    let numMax = array[0].paginas
+    let numMin = array[0].paginas
+    for (let index = 1; index < array.length; index++) {
+
+        if (array[index].paginas > numMax) { 
+            tituloLibroMasPaginas = array[index].titulo; 
+
+            numMax = array[index].paginas
+        }
+        if (array[index].paginas < numMin) {
+            tituloLibroMenosPaginas = array[index].titulo; 
+            numMin = array[index].paginas
+        }
+           
+    }
+    return console.log(`Libro con más páginas: ${tituloLibroMasPaginas} con ${numMax} y Libro con menos páginas: ${tituloLibroMenosPaginas} con ${numMin} `)
+
+}
+
+minMaxPagLibros([
+    { titulo: "El Hobbit", paginas: 310 },
+    { titulo: "Cien años de soledad", paginas: 417 },
+    { titulo: "1984", paginas: 328 },
+    { titulo: "Don Quijote de la Mancha", paginas: 863 },
+    { titulo: "Harry Potter y la piedra filosofal", paginas: 223 },
+    { titulo: "Matar un ruiseñor", paginas: 281},
+    { titulo: "Crimen y castigo", paginas: 671 },
+    { titulo: "Las aventuras de Sherlock Holmes", paginas: 307 },
+    { titulo: "El Señor de los Anillos: La Comunidad del Anillo", paginas: 423 },
+    { titulo: "El Gran Gatsby", paginas: 180 }
+])
+  
+function minAndMaxWord(words) { 
+    let newArrayMinMax = []
+    let numMax = words[0]
+    let numMin = words[0]
+    let indexMax = 0
+    let indexMin = 0
+    for (let index = 1; index < words.length; index++) {
+
+        if (words[index] > numMax) { 
+            indexMax = index
+            numMax = words[index]
+        }
+        if (words[index] < numMin) {
+            indexMin = index
+            numMin = words[index]
+        }
+           
+    }
+    newArrayMinMax.push(indexMax, indexMin)
+    return console.log(newArrayMinMax)
+
+}
+minAndMaxWord([310, 417, 328, 281, 863, 223, 671, 307, 423, 180])
+
+
+function minAndMaxWordSimple(words) { 
+    let arrayMaxMin = []
+    let indexMax = words.indexOf(Math.max(...words))
+    let indexMin = words.indexOf(Math.min(...words))
+    arrayMaxMin.push(indexMin, indexMax)
+    console.log(arrayMaxMin)
+    return arrayMaxMin
+}
+minAndMaxWordSimple([310, 417, 328, 281, 863, 223, 671, 307, 423, 180])
 
 
 
